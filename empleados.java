ebcb051ab42a61f26798d1c3901370f40bf79b47
BorjaMartinezRubio <47325350+BorjaMartinezRubio@users.noreply.github.com>
2019-02-20 17:56:37 +0100

    CreacionGestionPersonas

--- empleados.java
+++ empleados.java
@@ -1,10 +1,38 @@
 
-public class empleados extends personas{
-	
-	//Atributos
+public class empleados extends personas {
+
+	// Atributos
 	protected double antigüedad, salario;
-	protected String puesto; //¿Utilizamos puestos para que dependiendo de este pueda realizar cambios de unas cosas u otras?
-	protected String especialidad; //Especialista en coches, motos, camiones...
+	protected String puesto; // ¿Utilizamos puestos para que dependiendo de este pueda realizar cambios de
+								// unas cosas u otras?
+	protected String especialidad; // Especialista en coches, motos, camiones...
+	
+	
+	protected double getAntigüedad() {
+		return antigüedad;
+	}
+	protected void setAntigüedad(double antigüedad) {
+		this.antigüedad = antigüedad;
+	}
+	protected double getSalario() {
+		return salario;
+	}
+	protected void setSalario(double salario) {
+		this.salario = salario;
+	}
+	protected String getPuesto() {
+		return puesto;
+	}
+	protected void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+	protected String getEspecialidad() {
+		return especialidad;
+	}
+	protected void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+
 	
 
 }
